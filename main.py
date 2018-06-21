38cdf33f9d7ece4de389b8e6b98afbccdeb2caf6
Mark <markteams@gmail.com>
2018-06-21 16:09:35 +0300

    set_labels method added.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import twitter
 import os
 import json
 import errno
+from collections import defaultdict
 
 
 class TwiPy:
@@ -20,9 +21,9 @@ class TwiPy:
         if query == "":
             return False
 
-        self.tweets = self.api.GetSearch(term=query, count=count)
+        return self.api.GetSearch(term=query, count=count)
 
-    def export_tweets(self):
+    def export_tweets(self, raw_tweets):
         output_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'python_tweets.json')
 
         if not os.path.exists(os.path.dirname(output_filename)):
@@ -33,30 +34,52 @@ class TwiPy:
                     raise
 
         with open(output_filename, 'a') as output_file:
-            for tweet in self.tweets:
-                if tweet.text != "":
-                    output_file.write(json.dumps(tweet.text))
-                    output_file.write("\n\n")
+
+            tweets_list = dict((tweet.id, tweet.text) for tweet in raw_tweets if tweet.text != "")
+
+            output_file.write(json.dumps(tweets_list,
+                                         sort_keys=True,
+                                         indent=4))
 
     def import_tweets(self):
         input_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'python_tweets.json')
         labels_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'python_classes.json')
 
         with open(input_filename) as tweet_json_file:
-            for tweet in tweet_json_file:
-                if len(tweet.strip()) == 0:
-                    continue
-                self.tweets.append(json.loads(tweet))
+            self.tweets = json.loads(tweet_json_file.read())
 
-        labels = []
+        labels = defaultdict(int)
         if os.path.exists(labels_filename):
-            with open(labels_filename) as label_file:
+            with open(labels_filename, 'r') as label_file:
                 labels = json.load(label_file)
+
+        return labels
+
+    def set_labels(self):
+        labels_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'python_classes.json')
+        labels = self.import_tweets()
+
+
+        for tweet_idx in self.tweets.keys():
+            if tweet_idx in labels:
+                continue
+            print("Set the relevance to the topic for this tweet: [0: Not relevant, 1: Relevant]\n")
+            print(self.tweets[tweet_idx])
+            labels[tweet_idx] = int(input("Enter 0 or 1:"))
+            print('='*40)
+
+            with open(labels_filename, 'w') as labels_file:
+                labels_file.write(json.dumps(labels,
+                                             sort_keys=True,
+                                             indent=4))
+
 
 
 if __name__ == '__main__':
 
     twipy = TwiPy()
-    twipy.search_tweets('python')
-    twipy.export_tweets()
+    raw_tweets = twipy.search_tweets('python')
+    twipy.export_tweets(raw_tweets)
     twipy.import_tweets()
+    twipy.set_labels()
+
