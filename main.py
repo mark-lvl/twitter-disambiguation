192884e6986aadd5a9a49b65d2eeddcdaed71780
Mark <markteams@gmail.com>
2018-06-26 23:54:08 +0300

    argparser added to project, generalize Twipy methods

--- main.py
+++ main.py
@@ -1,40 +1,70 @@
 from twipy import TwiPy
-import pandas as pd
+import argparse
+from colorama import Fore, Back, Style, init
 from bagofwords import BagOfWords
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import cross_val_score,cross_val_predict
+from sklearn.model_selection import cross_val_score
 import numpy as np
-from sklearn.model_selection import GridSearchCV
+
 
 if __name__ == '__main__':
+    # reset color changes
+    init(autoreset=True)
+
+    parser = argparse.ArgumentParser(prog='main',
+                                     usage='%(prog)s func [options]',
+                                     description='Disambiguating the use of the given term  on Twitter stream.')
+    parser.add_argument(
+        'func',
+        choices=['fetch_tweets', 'label_tweets'],
+        help='The method name to call')
+    parser.add_argument(
+        '-p',
+        '--phrase',
+        nargs='?',
+
+        help='The phrase you want to disambiguate in tweets, only alphanumeric and hyphen.')
+    parser.add_argument(
+        '-c',
+        '--count',
+        nargs='?',
+        const=100,
+        default=100,
+        help='Number of tweets to return containing the given term'
+    )
+    # checks arguments dependencies
+    args = parser.parse_args()
+    if args.func in ['fetch_tweets', 'label_tweets'] and not args.phrase:
+        parser.error('For chosen function, -p argument is required.')
 
     twipy = TwiPy()
-    # raw_tweets = twipy.search_tweets('python')
-    # twipy.export_tweets(raw_tweets)
-    twipy.import_tweets()
-    # twipy.set_labels()
+    if args.func == 'fetch_tweets':
+        twipy.fetch_tweets(args.phrase, args.count)
 
-    tweets = twipy.get_tweets()
+    if args.func == 'label_tweets':
+        twipy.disambiguate_tweets(args.phrase)
 
-    pipeline = Pipeline([('bag-of-words', BagOfWords()),
-                         ('vectorizer', DictVectorizer()),
-                         ('naive-bayes', BernoulliNB())
-                         ])
-
+    # tweets = twipy.get_tweets()
+    #
+    # pipeline = Pipeline([('bag-of-words', BagOfWords()),
+    #                      ('vectorizer', DictVectorizer()),
+    #                      ('naive-bayes', BernoulliNB())
+    #                      ])
+    #
     # scores = cross_val_score(pipeline, tweets['Tweet'], tweets['Label'], scoring='f1')
     # print("Score: {:.3f}".format(np.mean(scores)))
-
-    model = pipeline.fit(tweets['Tweet'], tweets['Label'])
-
-    nb = model.named_steps['naive-bayes']
-    feature_probabilities = nb.feature_log_prob_
-
-    top_features = np.argsort(-nb.feature_log_prob_[1])[:50]
-
-    dv = model.named_steps['vectorizer']
-
-    for i, feature_index in enumerate(top_features):
-        print(i, dv.feature_names_[feature_index], np.exp(feature_probabilities[1][feature_index]))
-
+    #
+    # model = pipeline.fit(tweets['Tweet'], tweets['Label'])
+    #
+    # nb = model.named_steps['naive-bayes']
+    # feature_probabilities = nb.feature_log_prob_
+    #
+    # top_features = np.argsort(-nb.feature_log_prob_[1])[:50]
+    #
+    # dv = model.named_steps['vectorizer']
+    #
+    # for i, feature_index in enumerate(top_features):
+    #     print(i, dv.feature_names_[feature_index], np.exp(feature_probabilities[1][feature_index]))
+    #
--- twipy.py
+++ twipy.py
@@ -3,16 +3,22 @@ import twitter
 import config
 import os
 import json
+import re
 import errno
 from collections import defaultdict
 import pandas as pd
+from colorama import Fore, Back, Style, init
 
 
 class TwiPy:
     tweets: dict
     labels: dict
+    tweets_export_filename: str
 
     def __init__(self):
+        # reseting color changes in prints
+        init(autoreset=True)
+
         self.api = twitter.Api(consumer_key=config.TWITTER_CONFIG['consumer_key'],
                                consumer_secret=config.TWITTER_CONFIG['consumer_secret'],
                                access_token_key=config.TWITTER_CONFIG['access_token_key'],
@@ -20,39 +26,30 @@ class TwiPy:
 
         self.tweets = {}
         self.labels = {}
-        # file to keep raw tweets to process
-        self.tweets_export_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'tweets.json')
         # file to keep relevance labels of tweets
         self.labels_filename = os.path.join(os.path.curdir, 'output', 'twitter', 'tweets_labels.json')
 
-    def search_tweets(self, query, count=100):
-        if query == "":
+    def fetch_tweets(self, phrase, count=100, export=True):
+        # format the query phrase
+        phrase = re.findall("[a-zA-Z0-9-]+", phrase)
+        phrase = phrase[0].lower()
+
+        if phrase == "":
+            print(Fore.RED + 'Tweet phrase is empty.')
             return False
 
-        return self.api.GetSearch(term=query,
-                                  count=count,
-                                  result_type="recent",
-                                  lang='en')
+        raw_tweets = self.api.GetSearch(term=phrase,
+                                        count=count,
+                                        result_type="recent",
+                                        lang='en')
+        # if we don't want to export tweets we can terminate the function here and return fetched tweets
+        if not export:
+            return raw_tweets
 
-    def export_tweets(self, raw_tweets):
-        tweets = defaultdict()
+        tweets_filename, labels_filename, tweets, labels = self.file_loader(phrase)
 
-        # Check for directory existence
-        if not os.path.exists(os.path.dirname(self.tweets_export_filename)):
-            try:
-                os.makedirs(os.path.dirname(self.tweets_export_filename))
-            except OSError as exc:  # Guard against race condition
-                if exc.errno != errno.EEXIST:
-                    raise
-
-        # Load the current file in case of existence and having content
-        elif os.path.isfile(self.tweets_export_filename) and (os.path.getsize(self.tweets_export_filename) > 0):
-            with open(self.tweets_export_filename, 'r') as output_file:
-                # First append existing tweets to the tweet dictionary
-                tweets = dict(json.load(output_file))
-
-        # Append recent tweets to the output file
-        with open(self.tweets_export_filename, 'w') as output_file:
+        # Append recent fetched tweets to the tweets file
+        with open(tweets_filename, 'w') as output_file:
             tweets.update({int(tweet.id): tweet.text for tweet in raw_tweets if tweet.text != ""})
 
             # Convert all keys to int before encoding in json - it helps following sort
@@ -62,28 +59,51 @@ class TwiPy:
                                          sort_keys=True,
                                          indent=4))
 
-    def import_tweets(self):
+        print(Back.GREEN + Fore.BLACK + '{} tweets exported in {} successfully.'.format(len(tweets),
+                                                                                        tweets_filename))
 
-        with open(self.tweets_export_filename) as tweet_json_file:
-            self.tweets = json.loads(tweet_json_file.read())
+    def file_loader(self, phrase):
+        if not phrase:
+            return False
 
+        # Check for directory existence
+        tweets_filename = os.path.join(os.path.curdir, 'output', 'twitter', phrase + '_tweets.json')
+        if not os.path.exists(os.path.dirname(tweets_filename)):
+            try:
+                os.makedirs(os.path.dirname(tweets_filename))
+            except OSError as exc:  # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
+
+        # dictionary of all existing and recent fetched tweets
+        tweets = defaultdict()
+        # Load the current file in case of existence and having content
+        if os.path.isfile(tweets_filename) and (os.path.getsize(tweets_filename) > 0):
+            with open(tweets_filename, 'r') as tweet_file:
+                # First append existing tweets to the tweet dictionary
+                tweets = dict(json.load(tweet_file))
+
+        # Loading the label file and convert contents to dictionary
+        labels_filename = os.path.join(os.path.curdir, 'output', 'twitter', phrase + '_labels.json')
         labels = defaultdict(int)
-        if os.path.exists(self.labels_filename):
-            with open(self.labels_filename, 'r') as label_file:
-                self.labels = json.load(label_file)
+        if os.path.exists(labels_filename):
+            with open(labels_filename, 'r') as label_file:
+                labels = json.load(label_file)
 
-        return self.labels
+        return tweets_filename, labels_filename, tweets, labels
 
-    def set_labels(self):
+    def disambiguate_tweets(self, phrase):
 
-        labels = self.import_tweets()
+        tweets_filename, labels_filename, tweets, labels = self.file_loader(phrase)
 
-        for tweet_idx in self.tweets.keys():
+        for tweet_idx in tweets.keys():
+            # following statement means the tweet was already labels so we can jump over it
             if tweet_idx in labels:
                 continue
+
             print("Set the relevance to the topic for this tweet: [0: Not relevant, 1: Relevant]\n")
             print('=' * 40)
-            print(self.tweets[tweet_idx])
+            print(tweets[tweet_idx])
             print('=' * 40, '\n')
             # Getting value from terminal input and evaluate
             while True:
@@ -96,7 +116,7 @@ class TwiPy:
 
             os.system('clear')
 
-            with open(self.labels_filename, 'w') as labels_file:
+            with open(labels_filename, 'w') as labels_file:
                 labels_file.write(json.dumps(labels,
                                              sort_keys=True,
                                              indent=4))
